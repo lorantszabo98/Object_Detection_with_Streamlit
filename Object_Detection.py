570d17aa52d0e520e7381bd7c480821f65dcfef9
lorantszabo98 <64622519+lorantszabo98@users.noreply.github.com>
2023-12-07 12:12:18 +0100

    model selection, showing the results in a dataframe, time measurement, sending the results with Session state

--- Object_Detection.py
+++ Object_Detection.py
@@ -1,47 +1,21 @@
-# import streamlit as st
-# import cv2
-# import numpy as np
-# import tensorflow as tf
-#
-# st.title("Object Detection with Streamlit")
-#
-# # Load MobileNet SSD model and configuration
-# model_path = 'pages/models/ssd_mobilenet_v2_320_320/ssd_mobilenet_v2_320x320_coco17_tpu-8/saved_model/saved_model.pb'
-# # config_path = 'pages/models/ssd_mobilenet_v2_320_320/ssd_mobilenet_v2_320x320_coco17_tpu-8/pipeline.config'
-# #
-# # net = cv2.dnn.readNetFromTensorflow(model_path + '/saved_model.pb', config_path)
-#
-# model = tf.saved_model.load(model_path)
-#
-# cap = cv2.VideoCapture(0)
-#
-# if not cap.isOpened():
-#     st.error("Error: Could not open webcam.")
-# else:
-#     webcam_success_message = st.success("Webcam is active.")
-#
-# webcam_placeholder = st.empty()
-#
-# while cap.isOpened():
-#     ret, frame = cap.read()
-#
-#     if not ret:
-#         st.warning("Warning: Could not read a frame from the webcam.")
-#         break
-#
-#     webcam_placeholder.image(frame, channels="BGR", use_column_width=True)
-#
-# cap.release()
-
-from torchvision.models import detection
-import pickle
 import torch
+from torchvision.models import detection
 import cv2
 import numpy as np
 import streamlit as st
 from PIL import Image, ImageOps
+import pandas as pd
+import time
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+@st.cache_resource
+def load_models(model_name):
+    model = MODELS[model_name](pretrained=True, progress=True,
+                                   num_classes=len(CLASSES), pretrained_backbone=True).to(DEVICE)
+    # set inference mode
+    model.eval()
+
+    return model
 
 CLASSES = [
     'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
@@ -60,66 +34,112 @@ CLASSES = [
 
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 MODELS = {
     "frcnn-resnet": detection.fasterrcnn_resnet50_fpn,
     "frcnn-mobilenet": detection.fasterrcnn_mobilenet_v3_large_320_fpn,
     "retinanet": detection.retinanet_resnet50_fpn
 }
 
-model = MODELS["frcnn-resnet"](pretrained=True, progress=True,
-                               num_classes=len(CLASSES), pretrained_backbone=True).to(DEVICE)
-model.eval()
+# model = load_models("frcnn-resnet")
+# model = load_models("frcnn-mobilenet")
+# model = load_models("retinanet")
+
+# Create a session state to store the DataFrame
+if 'detection_df' not in st.session_state:
+    st.session_state.detection_df = pd.DataFrame()
 
 st.title("Object Detection with Streamlit")
 
-image = st.file_uploader("Please upload a photo of your face", accept_multiple_files=False, type=["jpg", "jpeg", "png"])
+st.info("To perform object detection, please upload a photo!")
 
-image_placeholder = st.empty
+image = st.file_uploader("Please upload a photo!", accept_multiple_files=False, type=["jpg", "jpeg", "png"])
+
+image_placeholder = st.empty()
+
 if image:
-    # image_placeholder = st.image(image, channels="BGR", use_column_width=True)
 
-    image = Image.open(image)
-    image = ImageOps.exif_transpose(image)
+    selected_model = add_selectbox = st.selectbox(
+        "Which model do you want to use for detection?",
+        ("frcnn-resnet", "frcnn-mobilenet", "retinanet"),
+        index=0
+    )
 
-    image = np.array(image)
+    if st.button("Perform object detection"):
 
-    orig = image.copy()
-    # orig = ImageOps.exif_transpose(orig)
+        model = load_models(selected_model)
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = image.transpose((2, 0, 1))
+        with st.spinner("Object detection in progress..."):
 
-    image = np.expand_dims(image, axis=0)
-    image = image / 255.0
-    image = torch.FloatTensor(image)
+            start_time = time.time()
 
-    image = image.to(DEVICE)
-    detections = model(image)[0]
+            image = Image.open(image)
+            image = ImageOps.exif_transpose(image)
 
-    # loop over the detections
-    for i in range(0, len(detections["boxes"])):
-        # extract the confidence (i.e., probability) associated with the
-        # prediction
-        confidence = detections["scores"][i]
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > 0.5:
-            # extract the index of the class label from the detections,
-            # then compute the (x, y)-coordinates of the bounding box
-            # for the object
-            idx = int(detections["labels"][i])
-            box = detections["boxes"][i].detach().cpu().numpy()
-            (startX, startY, endX, endY) = box.astype("int")
-            # display the prediction to our terminal
-            label = "{}: {:.2f}%".format(CLASSES[idx-1], confidence * 100)
-            print("[INFO] {}".format(label))
-            st.write(label)
-            # draw the bounding box and label on the image
-            cv2.rectangle(orig, (startX, startY), (endX, endY),
-                          COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(orig, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, COLORS[idx], 4)
+            image = np.array(image)
 
+            orig = image.copy()
+            # orig = ImageOps.exif_transpose(orig)
 
-    image_placeholder = st.image(orig, channels="RGB", use_column_width=True)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = image.transpose((2, 0, 1))
+
+            image = np.expand_dims(image, axis=0)
+            image = image / 255.0
+            image = torch.FloatTensor(image)
+
+            image = image.to(DEVICE)
+            detections = model(image)[0]
+
+            data_rows = []
+
+            # loop over the detections
+            for i in range(0, len(detections["boxes"])):
+                # extract the confidence (i.e., probability) associated with the
+                # prediction
+                confidence = detections["scores"][i]
+                # filter out weak detections by ensuring the confidence is
+                # greater than the minimum confidence
+                if confidence > 0.5:
+                    # extract the index of the class label from the detections,
+                    # then compute the (x, y)-coordinates of the bounding box
+                    # for the object
+                    idx = int(detections["labels"][i])
+                    box = detections["boxes"][i].detach().cpu().numpy()
+                    (startX, startY, endX, endY) = box.astype("int")
+                    # display the prediction to our terminal
+                    label = "{}: {:.2f}%".format(CLASSES[idx-1], confidence * 100)
+                    # st.write(label)
+                    # draw the bounding box and label on the image
+                    cv2.rectangle(orig, (startX, startY), (endX, endY),
+                                  COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    # sgow the label in the photo
+                    cv2.putText(orig, label, (startX, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.5, COLORS[idx], 4)
+
+                    # create dictionary to store label and confidence values
+                    detection_info = {
+                        'Label': CLASSES[idx-1],
+                        'Confidence': "{:.2f}%".format(float(confidence * 100)),
+                        # Add more fields as needed
+                    }
+
+                    # add dictionary to a list
+                    data_rows.append(detection_info)
+
+            # measure the detection time
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            st.toast(f"Object detection took {elapsed_time:.4f} seconds")
+
+        # show the image with detecton results
+        image_placeholder = st.image(orig, channels="RGB", use_column_width=True)
+
+        # show the detection results in a dataframe
+        df = pd.DataFrame(data_rows)
+        st.dataframe(df)
+
+        # add this dataframe to the Session state to perform further processing in the other page
+        st.session_state.detection_df = df
